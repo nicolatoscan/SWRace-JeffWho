7959d8c18f30c8e5a2422908c293832294371904
Nicola Toscan <nicolatoscan99@outlook.it>
2020-05-19 16:30:20 +0200

    Initi

---
+++ base.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <string>
+#include <algorithm>
+#include <ctime>
+#include <queue>
+
+using namespace std;
+
+#ifdef EVAL
+ifstream in("input.txt");
+ofstream out("output.txt");
+#else
+ifstream in("../input/input0.txt");
+ostream &out(cout);
+#endif
+
+int main() {
+
+    return 0;
+}
