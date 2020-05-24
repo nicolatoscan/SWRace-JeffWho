202fb035d4075a2c558fd6d352d65d45e543a42e
Lorenzo Godi <34336808+LorenzoGodi@users.noreply.github.com>
2020-05-24 02:31:26 +0200

    Non mi lascia Pullare

--- Godi/solver.cpp
+++ Godi/solver.cpp
@@ -13,17 +13,23 @@ using namespace std;
 #define PATH_VER 4
 #define PATH_HOR 8
 
-#define CURVE_1 16
-#define CURVE_2 32
-#define CURVE_3 64
-#define CURVE_4 128
+#define PATH_CURVE_1 16  //   |_
+#define PATH_CURVE_2 32  // _|
+#define PATH_CURVE_3 64  // ¯|
+#define PATH_CURVE_4 128 //   |¯
 
-#define UP 256
-#define DOWN 512
-#define LEFT 1024
-#define RIGHT 2048
+#define MOV_UP 256
+#define MOV_LEFT 512
+#define MOV_RIGHT 1024
+#define MOV_DOWN 2048
+#define MOVS 3840
+#define MOVV 2304
+#define MOVH 1536
 
-/* ------------------------------------------ */
+
+#define BORDER 4096
+
+/* -----------------------------------------¯- */
 
 typedef struct Angoli
 {
@@ -48,17 +54,14 @@ typedef struct Angoli
 /* ------------------------------------------ */
 
 // Temp
-int i, j, r, c;
+int temp, i, j, r, c;
 
 // Data
 int R, C, B, W, A;
+int R1, C1, R2, C2;
 
 // Matrice base con anelli
 int **mat;
-
-// Quanti anelli per ogni riga/colonna
-int *howmany_in_row;
-int *howmany_in_col;
 
 /* ------------------------------------------ */
 
@@ -72,12 +75,95 @@ ifstream in("../input/" + sfile);
 /* ------------------------------------------ */
 
 void load(void);
-void print_stats(void);
-Angoli* get_quadrato(void);
+void get_around(void);
 
-void find_path(void);
+/* ------------------------------------------ */
 
-void print_path_quadrato(Angoli a);
+int keep_ring(int n)
+{
+    return n & (RING_BLACK | RING_WHITE);
+}
+int keep_path(int n)
+{
+    return n & 252;
+}
+int keep_mov(int n)
+{
+    return n & 3840;
+}
+int mov_inv(int n)
+{
+    switch (n)
+    {
+        case MOV_UP: return MOV_DOWN;
+        case MOV_RIGHT: return MOV_LEFT;
+        case MOV_DOWN: return MOV_UP;
+        case MOV_LEFT: return MOV_RIGHT;
+    }
+}
+pair<int, int> mov_apply(int r, int c, int mov)
+{
+    if(mov == MOV_UP) return make_pair(r - 1, c);
+    else if(mov == MOV_DOWN) return make_pair(r + 1, c);
+    else if(mov == MOV_LEFT) return make_pair(r, c - 1);
+    else return make_pair(r, c + 1);
+}
+pair<int, int> mov_apply(pair<int,int> coord, int mov)
+{
+    return mov_apply(coord.first, coord.second, mov);
+}
+
+int prosegui(pair<int, int> coord, int last_move, bool ultimo_dritto, bool deve_dritto, bool deve_curvare, int depth)
+{
+    int r = coord.first, c = coord.second;
+
+    // Tolgo mossa inversa dalle possibili
+    int movs = MOVS & ~ mov_inv(last_move);
+
+    // Tolgo mosse che vanno fuori dalle celle possibili (Bordi e celle dove sono già stato)
+    if(mat[r-1][c] & BORDER) movs &= ~MOV_UP;
+    if(mat[r+1][c] & BORDER) movs &= ~MOV_DOWN;
+    if(mat[r][c-1] & BORDER) movs &= ~MOV_LEFT;
+    if(mat[r][c+1] & BORDER) movs &= ~MOV_RIGHT;
+    
+    // Rispetto regole
+    if(last_move & (MOV_UP | MOV_DOWN))
+    {
+        // Arrivo da mossa verticale e devo andare verticale
+        if(deve_dritto || mat[r][c] & RING_WHITE) movs &= ~(MOV_LEFT | MOV_RIGHT);
+
+        // Arrivo da mossa verticale e devo andare orizzontale
+        if(deve_curvare || mat[r][c] & RING_BLACK) movs &= ~(MOV_UP | MOV_DOWN);
+    }
+    else
+    {
+        // Arrivo da mossa orizzontale e devo andare orizzontale
+        if(deve_dritto || mat[r][c] & RING_WHITE) movs &= ~(MOV_UP | MOV_DOWN);
+
+        // Arrivo da mossa orizzontale e devo andare verticale
+        if(deve_curvare || mat[r][c] & RING_BLACK) movs &= ~(MOV_LEFT | MOV_RIGHT);
+    }
+    
+    // Impongo regole se sono su un ring
+    bool next_deve_dritto = mat[r][c] & RING_BLACK;
+    bool next_deve_curvare = mat[r][c] & RING_WHITE;
+
+    // Ramo morto
+    int ways = 0;
+
+    // Faccio mosse possibili
+    for(i = MOV_UP; i <= MOV_DOWN; i *= 2)
+    {
+        // Se mossa è consentita
+        if(i & movs)
+        {
+            // Ho trovato un modo valido per proseguire
+            ways++;
+
+            int boh = prosegui(mov_apply(r, c, i), i, i &)
+        }
+    }
+}
 
 /* ---------------------------------------------------------------------------- */
 
@@ -86,23 +172,19 @@ int main(void)
     cout << "FILE: \n# " + sfile << endl << endl;
 
     load();
-    // print_stats();
-
-    Angoli* ang = get_quadrato();
-    if(ang != NULL)
-    {
-        print_path_quadrato(*ang);
-    }
-
-
+    
     return 0;
 }
 
 /* ---------------------------------------------------------------------------- */
 
-void find_path(void)
+void get_around(void)
 {
+    // Parto da angolo top-left
+    mat[1][1] += PATH_CURVE_4;
 
+    // Vado giù
+    // down_left();
 }
 
 /* ------------------------------------------ */
@@ -112,23 +194,21 @@ void load(void)
     in >> R >> C >> B >> W;
     A = B + W;
 
-    mat = new int *[R];
-    howmany_in_row = new int[R];
-    howmany_in_col = new int[C];
+    R1 = R + 1;
+    C1 = C + 1;
+    R2 = R + 2;
+    C2 = C + 2;
+
+    mat = new int *[R2];
 
     // Inizializza
-    for (r = 0; r < R; r++)
+    for (r = 0; r < R2; r++)
     {
-        mat[r] = new int[C];
-        for (c = 0; c < C; c++)
+        mat[r] = new int[C2];
+        for (c = 0; c < C2; c++)
         {
-            mat[r][c] = EMPTY;
+            mat[r][c] = (r == 0 || c == 0 || r == R1 || c == C1) ? BORDER : EMPTY;
         }
-        howmany_in_row[r] = 0;
-    }
-    for (c = 0; c < C; c++)
-    {
-        howmany_in_col[c] = 0;
     }
 
     // Carica anelli da file
@@ -136,112 +216,62 @@ void load(void)
     {        
         in >> r >> c;
 
-        mat[r][c] = RING_BLACK;
-
-        howmany_in_row[r]++;
-        howmany_in_col[c]++;
+        mat[r+1][c+1] = RING_BLACK;
     }
     for (int i = 0; i < W; i++) // White
     {        
         in >> r >> c;
 
-        mat[r][c] = RING_WHITE;
-        howmany_in_row[r]++;
-        howmany_in_col[c]++;
+        mat[r+1][c+1] = RING_WHITE;
     }
-}
-
-void print_stats(void)
-{
-    int free_rows_count = 0;
-    int free_cols_count = 0;
-
-    for(int r = 0; r < R; r++)
-    {
-        if(howmany_in_row[r] == 0) free_rows_count++;
-    }
-    for(int c = 0; c < C; c++)
-    {
-        if(howmany_in_col[c] == 0) free_cols_count++;
-    }
-
-    int tot = R * C;
-    int freecount = free_rows_count*C + free_cols_count*R - free_rows_count*free_cols_count;
-    float rap = tot - freecount;
-
-    cout << "STATS:" << endl;
-    cout << "# Tot celle: " << tot << endl;
-    cout << "# Free_rows: " << free_rows_count << " su " << R << endl;
-    cout << "# Free_cols: " << free_cols_count <<  " su " << C << endl;
-    cout << "# Zone Occupate/Totale = " << rap/tot*100 << "%" << endl << endl;
-}
-
-Angoli* get_quadrato(void)
-{
-    if(B*2 != W) return NULL;    
-    Angoli* ang = new Angoli();
-
-    // Righe
-    r = -1;
-    
-    while(howmany_in_row[++r] == 0);
-
-    if(howmany_in_row[r] != 4) return NULL;
-    ang->first_row = r;
-    if(howmany_in_row[++r] != 2) return NULL;
-
-    while(howmany_in_row[++r] == 0);
-
-    if(howmany_in_row[r] != 2) return NULL;
-    if(howmany_in_row[++r] != 4) return NULL;
-    ang->last_row = r;
-
-    while(r + 1 < R && howmany_in_row[++r] == 0);
-    if(r + 1 != R) return NULL;
-
-
-    // Colonne
-    c = -1;
-    
-    while(howmany_in_col[++c] == 0);
-
-    if(howmany_in_col[c] != 4) return NULL;
-    ang->first_col = c;
-    if(howmany_in_col[++c] != 2) return NULL;
-
-    while(howmany_in_col[++c] == 0);
-
-    if(howmany_in_col[c] != 2) return NULL;
-    if(howmany_in_col[++c] != 4) return NULL;
-    ang->last_col = c;
-
-    while(c + 1 < C && howmany_in_col[++c] == 0);
-    if(c + 1 != C) return NULL;
-
-    return ang;
 }
 
 /* ------------------------------------------ */
 
-void print_path_quadrato(Angoli a)
+void show_path()
 {
-    cout << "SOLUTION:\n# Type: Quadrato\n# Starting Point: (R,C) (" << a.first_row << "," << a.first_col << ")\n# Path: ";
-    
-    for(c = a.first_col; c < a.last_col; c++)
+    cout << "\033[32m   ";
+    for (int c = 0; c < C; c++)
+        if (c < 10)
+            cout << "  " << c;
+        else
+            cout << " " << c;
+    cout << "\033[0m" << endl;
+
+    for (int r = 0; r < R; r++)
     {
-        cout << "R";
+        if (r < 10)
+            cout << "\033[32m  " << r << " \033[0m";
+        else
+            cout << "\033[32m " << r << " \033[0m";
+
+        for (int c = 0; c < C; c++)
+        {
+            int color = 31;
+            int n = redSol[r][c];
+            string ccc = "0";
+            if (n == MOV_UP)
+                ccc = "↑";
+            else if (n == MOV_DOWN)
+                ccc = "↓";
+            else if (n == MOV_LEFT)
+                ccc = "←";
+            else if (n == MOV_RIGHT)
+                ccc = "→";
+            else
+            {
+                ccc = "0";
+                color = 37;
+            }
+
+            if (mat[r][c] & RING_BLACK)
+                color = 32;
+            if (mat[r][c] & RING_WHITE)
+                color = 34;
+
+            cout << "\033[" << color << "m " << ccc << " \033[0m";
+        }
+        cout << endl;
     }
-    for(r = a.first_row; r < a.last_row; r++)
-    {
-        cout << "D";
-    }
-    for(c = a.first_col; c < a.last_col; c++)
-    {
-        cout << "L";
-    }
-    for(r = a.first_row; r < a.last_row; r++)
-    {
-        cout << "U";
-    }
-    cout << endl << endl;
+    cout << endl;
 }
